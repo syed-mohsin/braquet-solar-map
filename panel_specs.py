ae6311788f4792404b398adb774d6a9bc8c98b45
Taka Koizumi <takayuki.koizumi@gmail.com>
2016-03-29 23:58:05 -0400

    added length/width parameters to panelLayout function, dropdown will reflect the new specs in the panel_specs.py

--- panel_specs.py
+++ panel_specs.py
@@ -1,3 +1,8 @@
 def panel_specs():
-    data = {'panels':[{'model_name': 'Canadian Solar 235', 'wattage': 235, 'length': 1.2, 'width': 1.6}, {'model_name': 'Ying Li 250', 'wattage': 250, 'length': 1.2, 'width': 1.5}]}
+    data = {'panels':[
+    {'id':0, 'model_name': 'Canadian Solar 270/60 (CS6K)', 'wattage': 270, 'length': 0.992, 'width': 1.65}, 
+    {'id':1, 'model_name': 'Canadian Solar 320/72 (CS6X)', 'wattage': 320, 'length': 0.996, 'width': 1.972},
+    {'id':2, 'model_name': 'Ying Li 250/60 (YL250P-29b)', 'wattage': 250, 'length': 0.990, 'width': 1.640},
+    {'id':3, 'model_name': 'Ying Li 300/72 (YL300P-35b)', 'wattage': 300, 'length': 0.990, 'width': 1.960},
+    ]}
     return data
